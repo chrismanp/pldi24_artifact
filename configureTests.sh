a88253afc8891fee9f4f185d6273be8eb95941f8
chrismanp <chrismanp92@outlook.com>
2024-03-07 08:05:14 -0500

    Remove jemalloc command

--- configureTests.sh
+++ configureTests.sh
@@ -5,16 +5,6 @@ WORKERS="1 18"
 TAPIR_CC=clang
 TAPIR_CXX=clang++
 
-#TAPIR_CC=/afs/ece/project/seth_group/pakha/my-opencilk-repo/build/bin/clang
-#TAPIR_CXX=/afs/ece/project/seth_group/pakha/my-opencilk-repo/build/bin/clang++
-
-#/afs/ece/project/seth_group/pakha/my-opencilk-repo/build/bin/clang -ftapir=opencilk --opencilk-resource-dir=../../opencilk/cheetah/build/ -O3 fib.c -o fib
-
-#TAPIR_CC=/afs/ece/project/seth_group/pakha/uli/src/compiler/build/bin/clang
-#TAPIR_CXX=/afs/ece/project/seth_group/pakha/uli/src/compiler/build/bin/clang++
-
-#TAPIR_CC=/afs/ece/project/seth_group/pakha/opencilk/opencilk-project/build/bin/clang
-#TAPIR_CXX=/afs/ece/project/seth_group/pakha/opencilk/opencilk-project/build/bin/clang++
 
 if [ -z $REF_BASE ]; then
     if [ -z $TAPIR_BASE ]; then
@@ -48,27 +38,13 @@ else
 fi
 echo $TAPIR_LIB
 
-# ../../uli-runtime/ext_lib/libpfm_build/lib/libpfm.a"
-
 LAZYD_CILK_FLAG="-fforkd=lazy -ftapir=serial -mllvm -noinline-tasks=true -mllvm -experimental-debug-variable-locations=false -mllvm -disable-parallelepilog-insidepfor=true  -fuse-ld=lld -O3"
-
-
-#ULI_CILK_FLAG="$LAZYD_CILK_FLAG -mllvm -lazy-poll-lowering=unwind-ulifsim -mllvm -lazy-enable-proper-polling=0 -mllvm -lazy-set-maxgrainsize=2048 -Xclang -fpfor-spawn-strategy=1 -mllvm -lazy-set-maxinstpoll=1"
 
 ULI_CILK_FLAG="$LAZYD_CILK_FLAG  --opencilk-resource-dir=../../opencilk/cheetah/build/"
 
-#ULI_CILK_FLAG="-ftapir=serial -fuse-ld=lld ../../uli-runtime/ext_lib/libpfm_build/lib/libpfm.a -fpfor-spawn-strategy=1"
-
-#ULI_CILK_FLAG="$LAZYD_CILK_FLAG -mllvm -lazy-poll-lowering=nop -mllvm -lazy-disable-unwind-polling=true -mllvm -lazy-enable-proper-polling=0 -mllvm -lazy-set-maxgrainsize=2048 -fpfor-spawn-strategy=1 -mllvm -lazy-set-maxinstpoll=1"
-
-
-#ULI_CILK_FLAG="$LAZYD_CILK_FLAG -DSTATS_OVER_TIME -mllvm -lazy-poll-lowering=unwind-ulifsim -mllvm -lazy-enable-proper-polling=0 -mllvm -lazy-set-maxgrainsize=2048 -Xclang -fpfor-spawn-strategy=1 -mllvm -lazy-set-maxinstpoll=1 -mllvm -enable-poll-epoch=true -mllvm -lazy-enable-main-instrumentation=false"
 UIPI_CILK_FLAG="-fforkd=uli -ftapir=serial -fuse-ld=lld -mllvm -lazy-poll-lowering=nop -mllvm -disable-lazy-endisui=true  --opencilk-resource-dir=../../opencilk/cheetah/build/"
-#UIPI_CILK_FLAG="-fforkd=uli -ftapir=serial -fuse-ld=lld -mllvm -lazy-poll-lowering=nop -mllvm -enable-lazy-stuiclui=true"
 SIGUSR_CILK_FLAG="-fforkd=sigusr -ftapir=serial -fuse-ld=lld -mllvm -lazy-poll-lowering=nop -mllvm -disable-lazy-endisui=true "
 EAGERD_CILK_FLAG="-fforkd=eager -ftapir=serial -fuse-ld=lld -fno-omit-frame-pointer  -mno-omit-leaf-frame-pointer -mllvm -eager-set-maxgrainsize=2048 -mllvm -enable-poll-epoch=false"
-#EAGERD_CILK_FLAG="-fforkd=eager -ftapir=serial -fuse-ld=lld -fno-omit-frame-pointer  -mno-omit-leaf-frame-pointer -mllvm -enable-poll-epoch=true "
-#TAPIR_CILK_FLAG="-fcilkplus -mllvm -cilk-set-maxgrainsize=2048 -ldl"
 TAPIR_CILK_FLAG="-fopencilk --opencilk-resource-dir=../../opencilk/cheetah/build/ -ldl"
 
 REF_CILK_FLAG=-fcilkplus #-fdetach
@@ -76,18 +52,16 @@ GCC_CILK_FLAG=-fcilkplus
 
 SERIAL_CFLAGS="-Dcilk_for=for -Dcilk_spawn=  -Dcilk_sync=  -D_Cilk_for=for -D_Cilk_spawn=  -D_Cilk_sync= "
 REPORT_CFLAGS="-Rpass=.* -Rpass-analysis=.*"
-# REPORT_CFLAGS="-Rpass=loop-spawning"
 
 CILKSAN_LIB=$TAPIR_LIB
 CILKSAN_CFLAGS="-g -fsanitize=cilk"
-# CILKSAN_LDFLAGS="-lcilksan -L$CILKSAN_LIB"
 CILKSAN_LDFLAGS="-fsanitize=cilk"
 
 CILKSCALE_CFLAGS="-flto -fcsi"
 CILKSCALE_LDFLAGS="-flto -fuse-ld=gold -L$TAPIR_LIB"
 CILKSCALE_LDLIBS="-lclang_rt.cilkscale-x86_64"
 
-JEMALLOC_LDLIBS="-L`jemalloc-config --libdir` -Wl,-rpath,`jemalloc-config --libdir` -ljemalloc `jemalloc-config --libs`"
+#JEMALLOC_LDLIBS="-L`jemalloc-config --libdir` -Wl,-rpath,`jemalloc-config --libdir` -ljemalloc `jemalloc-config --libs`"
 
 C_COMPILER() {
     case $1 in
@@ -127,13 +101,6 @@ CXX_COMPILER() {
 
 RUN_ON_P_WORKERS() {
     P=$1
-    # echo "CILK_NWORKERS=$P setarch x86_64 -R taskset -c 1-$P numactl -i all ionice -c 2 -n 0 $2"
-    # CILK_NWORKERS=$P setarch x86_64 -R taskset -c 1-$P numactl -i all ionice -c 2 -n 0 $2
-
-    # ECE private machine does not have ionice command
-    #echo "CILK_NWORKERS=$P setarch x86_64 -R taskset -c 1-$P numactl -i all ionice -c 2 -n 0 $2"
-    #CILK_NWORKERS=$P setarch x86_64 -R taskset -c 1-$P numactl -i all ionice -c 2 -n 0 $2
-
     echo "CILK_NWORKERS=$P $2"
     CILK_NWORKERS=$P $2
 }
